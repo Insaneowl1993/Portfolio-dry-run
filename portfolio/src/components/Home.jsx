35be4922cfa774d8383a8f2fd658c8dad92a7fb4
Dale Lambert <inaneowl@gmail.com>
2024-04-25 19:28:47 +1200

    added contact page and smooth scroll options. ready to repo to portfoilio site.

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className= ' w-full h-screen bg-[#0a192f]'>
-        {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            {/* Title */}
-            <h1 className='text-4xl font-bold text-gray-300'>Hello, I'm <span className='text-red-400'>Dale Lambert</span></h1>
-            <p className='text-4xl font-bold text-gray-300'>I'm a Junior Software Developer</p>
-            <p className='text-xl text-gray-400 m-w-[700px]'>  looking to make my mark in the tech industry. 
-            I am currently improving my skills daily with a focus on full-stack development and applications.</p>
-            {/* Button */}
-            <div>
-            <button className='flex items-center px-6 py-3 my-2 border-2 group text-white hover:bg-red-400 duration-500'>
-                View my work
-                <span className='group-hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-3' />
-                </span>
-                
-            </button>
-            </div>
-            </div>
+    <div name="home" className=" w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        {/* Title */}
+        <h1 className="text-4xl font-bold text-gray-300">
+          Hello, I'm <span className="text-red-400">Dale Lambert</span>
+        </h1>
+        <p className="text-4xl font-bold text-gray-300">
+          I'm a Junior Software Developer
+        </p>
+        <p className="text-xl text-gray-400 m-w-[700px]">
+          {" "}
+          looking to make my mark in the tech industry. I am currently improving
+          my skills daily with a focus on full-stack development and
+          applications.
+        </p>
+        {/* Button */}
+        <div>
+          <button className="flex items-center px-6 py-3 my-2 border-2 group text-white hover:bg-red-400 duration-500">
+            <Link
+              to="work"
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              View my Work
+            </Link>
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React, {useState} from "react";
-import { FaBars, FaTimes, FaGithub, FaLinkedin, } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
@@ -13,62 +14,108 @@ const Navbar = () => {
 
       {/* {Menu} */}
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to="home" smooth={true} offset={50} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={50} duration={500}>
+            About
+          </Link>
+          </li>
+        <li>
+          <Link to="skills" smooth={true} offset={50} duration={500}>
+            Skills
+          </Link></li>
+        <li>
+          <Link to="work" smooth={true} offset={50} duration={500}>
+            Work
+          </Link></li>
+        <li>
+          <Link to="contact" smooth={true} offset={50} duration={500}>
+            Contact
+          </Link></li>
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile menu */}
-      <ul className={! nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} offset={50} duration={500}>
+            Home
+          </Link></li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} offset={50} duration={500}>
+            Work
+          </Link></li>
+        <li className="py-6 text-4xl">
+        <Link onClick={handleClick} to="contact" smooth={true} offset={50} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-            <li className ='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 p-3' >
-                <a 
-                className='flex justify-between items-center w-full text-gray-300' 
-                href='/'>
-                    Linkedin
-                    <FaLinkedin size={30} />
-                </a>
-            </li>
-            <li className ='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]' >
-                <a 
-                className='flex justify-between items-center w-full text-gray-300' 
-                href='/'>
-                    Github
-                    <FaGithub size={30} />
-                </a>
-            </li>
-            <li className ='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]' >
-                <a 
-                className='flex justify-between items-center w-full text-gray-300' 
-                href='/'>
-                    Email
-                    <HiOutlineMail size={30} />
-                </a>
-            </li>
-            <li className ='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]' >
-                <a 
-                className='flex justify-between items-center w-full text-gray-300' 
-                href='/'>
-                    Resume
-                    <BsFillPersonLinesFill size={30} />
-                </a>
-            </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 p-3">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/dale-lambert-46b29267/"
+            >
+              Linkedin
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/Insaneowl1993"
+            >
+              Github
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto: ddlambert1@hotmail.com"
+            >
+              Email
+              <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              download=""
+              href="portfolio\src\assets\Dale_Lambert_CV.docx"
+            >
+              Resume
+              <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
         </ul>
       </div>
     </div>
